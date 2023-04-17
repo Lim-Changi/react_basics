06b6fbbc4532d968fa748adff45df0eeac365484
찬기 <limchangi@naver.com>
2023-04-17 18:13:34 +0900

    feat: Signup Router 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {
+    BrowserRouter, Routes, Route, Navigate,
+} from "react-router-dom";
 import SignupPage from "./pages/signup/Signup.page";
 
 function App() {
   return (
-      <SignupPage />
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Navigate to="/signup" />} />
+            <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+
   );
 }
 
